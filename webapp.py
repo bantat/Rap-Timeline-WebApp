9634c72fdeb926a3ed44d6711b5b1f557ae05321
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-04-22 21:58:40 -0500

    Added wiki link to webapp and fixed year difference.

--- webapp.py
+++ webapp.py
@@ -16,15 +16,24 @@ def displayHomePage(year, homepage_filename):
         homepage = f.read()
         f.close()
     except Exception, e:
-        hoempage = "Cannot read template file <tt>%s</tt>." % (homepage_filename)
+        homepage = "Cannot read template file <tt>%s</tt>." % (homepage_filename)
     
     page_content = {}
     year_report = ""
+    
     if year:
+    	int_year = int(year)
+    	difference = 2015 - int_year
+    	string_difference = str(difference)
         year_report = "<p>The year you chose was %s.</p>\n" % (year)
-        year_report += "<p>That was %s years ago.</p>\n" % (str(2015 - int(year)))
+        year_report += "<p>That was %s years ago.</p>\n" % (string_difference)
     year_report = indent(year_report, 1)
     page_content["results"] = year_report
+    wiki_link = '<p>Interested in learning more about the year %s?\n' % (year)
+    wiki_link += 'Follow this link to visit the wikipedia page for %s.\n' % (year)
+    wiki_link += '<p><a href="en.wikipedia.org/wiki/%s"> %s </a></p>\n' % (year, year)
+    wiki_link = indent(wiki_link, 1)
+    page_content["links"] = wiki_link
     output_html = homepage.format(**page_content)
     print "Content-type: text/html\r\n\r\n",
     print output_html
