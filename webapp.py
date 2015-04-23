2aad14207404f3b0464c9f4537b51ad91eecbf83
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-04-22 22:19:30 -0500

    Ive never been closer before.

--- webapp.py
+++ webapp.py
@@ -31,7 +31,7 @@ def displayHomePage(year, homepage_filename):
         wiki_link = '<p>Interested in learning more about the year %s?\n' % (year)
     	wiki_link += 'Here is a link to the wikipedia page for %s:\n' % (year)
     	wiki_link += '<a href="http://en.wikipedia.org/wiki/%s">' + \
-    	'Wikipedia </a></p>\n' (year)
+    	'Wikipedia </a></p>\n' % (year)
     wiki_link = indent(wiki_link, 1)
     year_report = indent(year_report, 1)
     page_content["results"] = year_report
