40145e2feb37c6b2890a0438b84fa728edfce605
Tore Banta <torebanta@res-230-200.dyn.carleton.edu>
2015-05-11 17:28:50 -0500

    Minor changes

--- DataSource.py
+++ DataSource.py
@@ -9,7 +9,7 @@ import sys
 
 class DataSource:
 
-    # TODO(Tore) Add try catch blocks
+    # TODO(Grader) We were unable to access our password for this phase of the project
     def __init__(self):
         USERNAME = 'bantat'
         DB_NAME = 'bantat'
@@ -42,13 +42,16 @@ class DataSource:
 
     # TODO(Tore) Check cursor returns something
     def getArtist(self, artist_name):
-        #sets the select statement for the artist we want
+        """Takes an artist name string as an argument, and returns an artist object containing all data about that
+        artist."""
+
+        # Sets the select statement for the artist we want
         sql_string = self.cursor.mogrify("SELECT * FROM artists WHERE name = %s;",(artist_name,))
         self.cursor.execute(sql_string)
-        #the list should only be one item with the name, description, and image path as 0, 1, and 2 respectively
+        # The list should only be one item with the name, description, and image path as 0, 1, and 2 respectively
         info = list(self.cursor.fetchone())
 
-        #this makes a list of items that has the names of the albums, and then makes album object for each album
+        # This makes a list of items that has the names of the albums, and then makes album object for each album
         album_names = []
         sql_string1=self.cursor.mogrify("SELECT name FROM albums WHERE artist = %s;",(artist_name,))
         self.cursor.execute(sql_string1)
@@ -61,39 +64,47 @@ class DataSource:
             album_object = self.getAlbum(album)
             album_objects.append(album_object)
 
-        #creates an object of the artist class, and then returns it
+        # Creates an object of the artist class, and then returns it
         artist_objects = Artist(info[0],info[1],info[2],album_objects)
 
         return artist_objects
 
     def getAlbum(self, album_name):
-        #selects the one album in the database that should have the same name.
+        """Takes an album name string as an argument, and returns an album object containing all data about that
+        album."""
+
+        # Selects the one album in the database that should have the same name.
         sql_string = self.cursor.mogrify("SELECT * FROM albums WHERE name = %s;",(album_name,))
         self.cursor.execute(sql_string)
-        #converts the database information into a list
+        # Converts the database information into a list
         album_info = list(self.cursor.fetchone())
-        #creates an object of the album class, and returns it
+        # Creates an object of the album class, and returns it
         album_object = Album(album_info[0],album_info[1],album_info[2],album_info[3],album_info[4])
 
         return album_object
 
     def getYearsOnTimeline(self):
-        #selects every year in the database
+        """Returns a list of integer year values for which there exists data."""
+
+        # Selects every year in the database
         self.cursor.execute("SELECT year FROM albums")
         years = []
 
-        #selects the integers from the tuples in the cursor
+        # Selects the integers from the tuples in the cursor
         for row in self.cursor:
             years.append(int(row[0]))
-        #turns the list into a set to eliminate doubles, and then reverses the order because we want to start 
-        #in the year we are in.
+        # Turns the list into a set to eliminate doubles, and then reverses the order because we want to start
+        # In the year we are in.
         years = list(set(years))
         years = reversed(years)
 
         return years
 
     def getYear(self, year):
-        #selects the name of the albums with the year that is passed, and places them in album_names
+        """Takes an integer year value as an argument, and returns a year object containing album objects for albums
+        which were released during that year."""
+
+        # Selects the name of the albums with the year that is passed, and places them in album_names
         self.cursor.execute("SELECT name FROM albums WHERE year= %s;",(year,))
         album_names = []
 
@@ -101,11 +112,11 @@ class DataSource:
             album_names.append(item[0])
 
         album_objects = []
-        #turns each album name into an album object
+        # Turns each album name into an album object
         for album in album_names:
             album_object = self.getAlbum(album)
             album_objects.append(album_object)
-        #creates a timeline object, and returns it
+        # Creates a timeline object, and returns it
         timeline_object = Timeline(year, album_objects)
 
         return timeline_object
@@ -113,9 +124,9 @@ class DataSource:
 
 class Album:
     """This class serves as the class that allows other modules to access information particular to one 
-    album
-    """
-    #constructs an album with the following information
+    album."""
+
+    # Constructs an album with the following information
     def __init__(self, album_name, album_description, album_image, album_year, album_artist):
         self.album_name = album_name
         self.album_description = album_description
@@ -123,11 +134,11 @@ class Album:
         self.album_image = album_image
         self.album_artist = album_artist
 
-    #returns the name of the album on the wikipedia page
+
     def getAlbumName(self):
         return self.album_name
 
-    #returns a string of the album without any qualifiers from the wikipedia
+
     def getAlbumString(self):
         album_string = self.album_name
 
@@ -152,8 +163,8 @@ class Album:
 
 class Artist:
     """This class serves as the class that will allow other modules to access information particular to one 
-    artist
-    """
+    artist."""
+
     #constructs an artist object with this information stored
     def __init__(self, artist_name, artist_description, artist_image, artist_albums):
         self.artist_name = artist_name
@@ -184,8 +195,8 @@ class Artist:
 
 
 class Timeline:
-    """This class serves to build a timeline based on one year. This stores the ablum objects for that particular year
-    """
+    """This class serves to build a timeline based on one year. This stores the ablum objects for that particular
+    year."""
 
     def __init__(self, year, albums):
         self.year = year
