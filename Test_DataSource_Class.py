1dbde414d60901ff7633ad2443867393850e0aca
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-05-14 22:04:21 -0500

    minor changes

--- Test_DataSource_Class.py
+++ Test_DataSource_Class.py
@@ -10,7 +10,7 @@ class TestDataSource(TestCase):
         data = DataSource.DataSource()
         artist = data.getArtist("Kanye West")
         image = artist.getArtistImage()
-        TestCase.assertEqual(image,'http://upload.wikimedia.org/wikipedia/commons/c/cd/'
+        TestCase.assertEqual(self, image,'http://upload.wikimedia.org/wikipedia/commons/c/cd/'
                                          'Kanye_West_Lollapalooza_Chile_2011_2.jpg',"Image path string does not match "
                                                                                     "string returned by getArtistImage")
 
@@ -18,7 +18,7 @@ class TestDataSource(TestCase):
         data = DataSource.DataSource()
         album = data.getAlbum("Yeezus")
         image = album.getAlbumImage()
-        TestCase.assertEqual(image,'http://upload.wikimedia.org/wikipedia/en/8/83/Yeezus_Kanye_West.jpg',
+        TestCase.assertEqual(self, image,'http://upload.wikimedia.org/wikipedia/en/8/83/Yeezus_Kanye_West.jpg',
                              "Image path string does not match string returned by getArtistImage")
 
     def verifyDatabaseConnections(self):
@@ -41,10 +41,10 @@ class TestDataSource(TestCase):
             error_description = "Missing album description for %s" % (album.getAlbumName())
             error_description2 = "Invalid album description for %s" % (album.getAlbumName())
             error_image = "Missing album image for %s" % (album.getAlbumName())
-            TestCase.assertTrue(len(album.getAlbumDescription()) > 0, error_description)
-            TestCase.assertTrue(len(album.getAlbumImage()) > 0, error_image)
-            TestCase.assertFalse('}' in album.getAlbumDescription(), error_description2)
-            TestCase.assertFalse('{' in album.getAlbumDescription(), error_description2)
+            TestCase.assertTrue(self, len(album.getAlbumDescription()) > 0, error_description)
+            TestCase.assertTrue(self, len(album.getAlbumImage()) > 0, error_image)
+            TestCase.assertFalse(self, '}' in album.getAlbumDescription(), error_description2)
+            TestCase.assertFalse(self, '{' in album.getAlbumDescription(), error_description2)
 
     def verifyDatabaseArtists(self):
         data = DataSource.DataSource()
@@ -53,10 +53,10 @@ class TestDataSource(TestCase):
             error_description = "Missing artist description for %s" % (artist.getArtistName())
             error_description2 = "Invalid artist description for %s" % (artist.getArtistName())
             error_image = "Missing artist image for %s" % (artist.getArtistName())
-            TestCase.assertTrue(len(artist.getArtistDescription()) > 0, error_description)
-            TestCase.assertTrue(len(artist.getArtistImage()) > 0, error_image)
-            TestCase.assertFalse('}' in artist.getArtistDescription(), error_description2)
-            TestCase.assertFalse('{' in artist.getArtistDescription(), error_description2)
+            TestCase.assertTrue(self, len(artist.getArtistDescription()) > 0, error_description)
+            TestCase.assertTrue(self, len(artist.getArtistImage()) > 0, error_image)
+            TestCase.assertFalse(self, '}' in artist.getArtistDescription(), error_description2)
+            TestCase.assertFalse(self, '{' in artist.getArtistDescription(), error_description2)
 
 if __name__ == '__main__':
     unittest.main()
