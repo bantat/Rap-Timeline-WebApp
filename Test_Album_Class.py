913dcd004bda1da800dcca584fbd905beb7d232b
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-05-17 17:39:07 -0500

    minor change

--- Test_Album_Class.py
+++ Test_Album_Class.py
@@ -8,19 +8,19 @@ __author__ = 'Tore Banta & Charlie Sarano'
 class TestAlbum(TestCase):
     def test_getAlbumName(self):
         album_object = DataSource.Album('Yeezus (album)',"Kanye's greatest accomplishment.",'yeezus.jpg',2013,
-                                        'Kanye West')
+                                        'Kanye West','NONE')
         TestCase.assertEqual(self,album_object.getAlbumName(),'Yeezus (album)', "Album name does not match string "
                                                                                 "returned by getAlbumName")
 
     def test_getAlbumString(self):
         album_object = DataSource.Album('Yeezus (album)',"Kanye's greatest accomplishment.",'yeezus.jpg',2013,
-                                        'Kanye West')
+                                        'Kanye West','NONE')
         TestCase.assertEqual(self,album_object.getAlbumString(),'Yeezus', "Album string representation does not match "
                                                                           "string returned by getAlbumName")
 
     def test_getAlbumDescription(self):
         album_object = DataSource.Album('Yeezus (album)',"Kanye's greatest accomplishment.",'yeezus.jpg',2013,
-                                        'Kanye West')
+                                        'Kanye West','NONE')
         TestCase.assertEqual(self,album_object.getAlbumDescription(),"Kanye's greatest accomplishment.", "Album "
                                                                                                          "description "
                                                                                                          "does not "
@@ -31,19 +31,19 @@ class TestAlbum(TestCase):
 
     def test_getAlbumYear(self):
         album_object = DataSource.Album('Yeezus (album)',"Kanye's greatest accomplishment.",'yeezus.jpg',2013,
-                                        'Kanye West')
+                                        'Kanye West','NONE')
         TestCase.assertEqual(self,album_object.getAlbumYear(),2013, "Year value does not match int returned by "
                                                                     "getAlbumName")
 
     def test_getAlbumImage(self):
         album_object = DataSource.Album('Yeezus (album)',"Kanye's greatest accomplishment.",'yeezus.jpg','2013',
-                                        'Kanye West')
+                                        'Kanye West','NONE')
         TestCase.assertEqual(self,album_object.getAlbumImage(),'yeezus.jpg', "Album image string does not match string"
                                                                              " returned by getAlbumImage")
 
     def test_getAlbumArtist(self):
         album_object = DataSource.Album('Yeezus (album)',"Kanye's greatest accomplishment.",'yeezus.jpg','2013',
-                                        'Kanye West')
+                                        'Kanye West','NONE')
         TestCase.assertEqual(self,album_object.getAlbumArtist(),'Kanye West', "Artist string does not match string"
                                                                               " returned by getAlbumArtist")
 
