0012333db11575685b81845bf20cb727cfd5981b
Tore Banta <torebanta@eduroam-160-90.dyn.carleton.edu>
2015-05-17 18:19:02 -0500

    Cleaned up model.py, commented

--- model.py
+++ model.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
 
-__author__ = 'Tore Banta & Charlie Sarano'
+"""A module used for generating the necessary content to be used in creating web pages, this module contains methods
+for the Model component of our web application.
+
+By Tore Banta & Charlie Sarano
+CS 257 - Software Design (Jadrian Miles)
+Carleton College
+"""
+
+__author__ = 'Tore Banta, Charlie Sarano'
 
 import view
 import DataSource
 
 
-def buildPageBasedOnParameters(parameters):
-    """This method takes a dictionary of strings as an argument, and returns a string containing the complete HTML text
-    for a web page, based on which parameters exist within the dictionary that is passed."""
+def buildPageBasedOnParameters(cgi_parameters):
+    """Takes a dictionary of Common Gateway Interface parameter strings as an argument, and returns a string containing
+    the complete HTML text for a web page, based on which parameters exist within the dictionary that is passed."""
 
     data_source = DataSource.DataSource()
 
     page_content = ""
 
-    if parameters['year'] != '':
-        year = parameters['year']
+    if cgi_parameters['year'] != '':
+        year = cgi_parameters['year']
 
         if int(year) in data_source.getYearsOnTimeline():
             page_content += view.buildHeaderPage()
@@ -30,8 +37,10 @@ def buildPageBasedOnParameters(parameters):
             page_content += view.buildYearsMenu(data_source.getYearsOnTimeline())
             page_content += view.buildTimelinePage(data_source.getYearsOnTimeline())
             page_content += view.buildFooterPage()
-    elif parameters['artist'] != '':
-        artist = parameters['artist']
+
+    elif cgi_parameters['artist'] != '':
+        artist = cgi_parameters['artist']
+
         artist_objects = data_source.getAllArtistsFromDatabase()
         artist_names = []
         for artist_object in artist_objects:
@@ -48,8 +57,10 @@ def buildPageBasedOnParameters(parameters):
             page_content += view.buildYearsMenu(data_source.getYearsOnTimeline())
             page_content += view.buildTimelinePage(data_source.getYearsOnTimeline())
             page_content += view.buildFooterPage()
-    elif parameters['album'] != '':
-        album = parameters['album']
+
+    elif cgi_parameters['album'] != '':
+        album = cgi_parameters['album']
+
         album_objects = data_source.getAllAlbumsFromDatabase()
         album_names = []
         for album_object in album_objects:
@@ -65,11 +76,14 @@ def buildPageBasedOnParameters(parameters):
             page_content += view.buildYearsMenu(data_source.getYearsOnTimeline())
             page_content += view.buildTimelinePage(data_source.getYearsOnTimeline())
             page_content += view.buildFooterPage()
-    elif parameters['search'] != '':
-        search_string = parameters['search']
+
+    elif cgi_parameters['search'] != '':
+        search_string = cgi_parameters['search']
+
         page_content += view.buildHeaderPage()
         page_content += view.buildSearchResultsPage(getSearchResults(search_string),search_string)
         page_content += view.buildFooterPage()
+
     else:
         page_content += view.buildHeaderPage()
         page_content += view.buildYearsMenu(data_source.getYearsOnTimeline())
@@ -80,7 +94,7 @@ def buildPageBasedOnParameters(parameters):
 
 
 def getAlbumContent(album_name):
-    """This method takes an album name string as an argument, and builds a dictionary of strings containing content to
+    """Takes an album name string as an argument, and returns a dictionary of strings containing content to
     be used in creating the album page."""
 
     data = DataSource.DataSource()
@@ -101,8 +115,8 @@ def getAlbumContent(album_name):
 
 
 def getArtistContent(artist_name):
-    """This method takes an artist name string as an argument, and builds a dictionary of strings containing content to
-    be used in creating the artist page."""
+    """Takes an artist name string as an argument, and returns a list of dictionaries containing content to be used in
+    creating the artist page."""
 
     data = DataSource.DataSource()
 
@@ -128,8 +142,8 @@ def getArtistContent(artist_name):
 
 
 def getYearContent(year):
-    """This method takes an integer year value as an argument, and builds a dictionary of strings containing content to
-    be used in creating the year page."""
+    """Takes an integer year value as an argument, and returns a list of dictionaries containing content to be used in
+    creating the year page."""
 
     data = DataSource.DataSource()
 
@@ -139,15 +153,13 @@ def getYearContent(year):
     albums = year_object.getAlbumsForYear()
     for album in albums:
         content_dictionary = {'album_id':'','album_name':'','album_img':'','artist_name':'','artist_id':'','year':''}
-        # print "Adding album: " + album.getAlbumString()
         content_dictionary['album_name'] = album.getAlbumString()
         content_dictionary['album_id'] = album.getAlbumName()
         content_dictionary['album_img'] = album.getAlbumImage()
-        # print "Artist name: " + album.getAlbumArtist()
+
         artist_name = album.getAlbumArtist()
-        # print "CREATING ARTIST OBJECT FROM " + artist_name
         artist_object = data.getArtist(artist_name)
-        # print "Artist string: " + artist_object.getArtistString()
+
         content_dictionary['artist_name'] = artist_object.getArtistString()
         content_dictionary['artist_id'] = artist_object.getArtistName()
         content_dictionary['year'] = year
@@ -155,20 +167,25 @@ def getYearContent(year):
 
     return content_list
 
+
 def getSearchResults(search_string):
+    """Takes a query string that has been provided by the user as an argument, and returns a list of two lists, the
+    first containing album object results and the second containing artist object results."""
 
     data_source = DataSource.DataSource()
-    album_objects= data_source.getAllAlbumsFromDatabase()
-    artist_objects=data_source.getAllArtistsFromDatabase()
+    album_objects = data_source.getAllAlbumsFromDatabase()
+    artist_objects = data_source.getAllArtistsFromDatabase()
     album_names = []
     artist_names = []
+
     for album in album_objects:
         album_names.append(album.getAlbumString().lower())
     for artist in artist_objects:
         artist_names.append(artist.getArtistString().lower())
 
-    artist_results=[]
-    album_results=[]
+    artist_results = []
+    album_results = []
+
     if " " not in search_string:
         for x in range(len(album_names)):
             if search_string in album_names[x]:
@@ -186,13 +203,19 @@ def getSearchResults(search_string):
             for x in range(len(artist_names)):
                 if word in artist_names[x]:
                     artist_results.append(artist_objects[x])
+
     artist_results = list(set(artist_results))
     album_results = list(set(album_results))
     
-    total_results= [album_results,artist_results]
+    total_results = [album_results, artist_results]
+
     return total_results
 
+
 def cleanDescription(description):
+    """Takes a description string, and returns a description string that has been properly formatted for display as HTML
+     text."""
+
     description_edit = description
     for i in range(len(description)):
         if description[i] == '^':
@@ -209,21 +232,3 @@ def cleanDescription(description):
         description = description.replace("\\",'')
 
     return description
-
-def main():
-    year_content = getYearContent(2012)
-    for dictionary in year_content:
-        for key, value in dictionary.iteritems():
-            print "['%s','%s']" % (str(key),str(value))
-
-    artist_content = getArtistContent("Isaiah Rashad")
-    for dictionary in artist_content:
-        for key, value in dictionary.iteritems():
-            print "['%s','%s']" % (str(key),str(value))
-
-    album_content = getAlbumContent("My Name Is My Name")
-    for key, value in album_content.iteritems():
-        print "['%s','%s']" % (str(key),str(value))
-
-# if __name__ == "__main__":
-#     main()
